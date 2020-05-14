a10aaeaab8067be39b005e2340c8986892f7ad1e
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-05-14 18:47:46 +0530

    Error Boundry added

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundery';
 import './App.css';
 
 class App extends Component {
@@ -35,7 +36,9 @@ class App extends Component {
           <h1 className='f1'>My Batch</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+                  <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
